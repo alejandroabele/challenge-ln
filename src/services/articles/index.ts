65c1cc0526e34aba4c9585f1aaf5b1bdcaed777e
aaejandroabele <alejandroabele@hotmail.com.ar>
2024-06-08 14:19:41 -0300

    feat: added fetch bff

--- src/services/articles/index.ts
+++ src/services/articles/index.ts
@@ -1,10 +1,11 @@
+import { fetchBff } from '@/utils/fetch'
 async function fetchArticles() {
-
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BFF_API_ROUTE}/articles`, { cache: 'no-store' })
-    if (!res.ok) {
-        throw new Error('Failed to fetch data')
+    try {
+        const res = await fetchBff(`/articles`)
+        return res
+    } catch (error) {
+        throw error
     }
-    return res.json()
 
 }
 
--- src/services/tags/index.ts
+++ src/services/tags/index.ts
@@ -1,9 +1,12 @@
+import { fetchBff } from '@/utils/fetch'
+
 async function fetchTags() {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BFF_API_ROUTE}/tags`, { cache: 'no-store' })
-    if (!res.ok) {
-        throw new Error('Failed to fetch data')
+    try {
+        const res = await fetchBff("/tags")
+        return res
+    } catch (error) {
+        throw error
     }
-    return res.json()
 }
 
 export {
