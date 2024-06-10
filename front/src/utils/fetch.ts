ca2f92c02b722327555e5c18a2bec3fca62b358a
aaejandroabele <alejandroabele@hotmail.com.ar>
2024-06-10 09:03:52 -0300

    fix: added cache store

--- front/src/utils/fetch.ts
+++ front/src/utils/fetch.ts
@@ -1,6 +1,6 @@
 const fetchBff = async (url: string, options = {}) => {
     const baseUrl = process.env.NEXT_PUBLIC_BFF_API_ROUTE;
-    const res = await fetch(`${baseUrl}${url}`, { ...options, cache: 'no-store' });
+    const res = await fetch(`${baseUrl}${url}`, { ...options });
     if (!res.ok) {
         throw new Error('Failed to fetch data');
     }
