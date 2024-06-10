9ad1230cf9a41eef4cff8cef3f849ea6954cc2e3
aaejandroabele <alejandroabele@hotmail.com.ar>
2024-06-10 10:20:43 -0300

    fix: fixed fetch function

--- front/src/utils/fetch.ts
+++ front/src/utils/fetch.ts
@@ -1,6 +1,6 @@
 const fetchBff = async (url: string, options = {}) => {
     const baseUrl = process.env.NEXT_PUBLIC_BFF_API_ROUTE;
-    const res = await fetch(`${baseUrl}${url}`, { ...options });
+    const res = await fetch(`${baseUrl}${url}`, { ...options, cache: 'no-store' });
     if (!res.ok) {
         throw new Error('Failed to fetch data');
     }
